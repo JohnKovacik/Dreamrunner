47728b9b9739c9554f8603476738d0fd3c3cc204
JohnKovacik <jkovacik@gmail.com>
2021-01-23 09:46:14 -0600

    File restructure

--- dreamrunner.js
+++ dreamrunner.js
@@ -1,21 +1,7 @@
 
 var encounterState = '';
 
-var dictPracticedManuvers = {
-	1 : "Fire Bolt", 
-	2 : "Cold Ray", 
-	3 : "Bellow of Rage", 
-	4 : "Shield of Conviction", 
-	5 : "Invisibility", 
-};
 
-var dictPracticedManuversStatus = {
-	1 : "<b>Cost:</b> 20 Lucidity<br /><b>Effect:</b> Strong Level 8 Fire Attack<br /><b>Result:</b> 24d6+24 (<i>Est. 108</i>) Fire Damage", 
-	2 : "<b>Cost:</b> 20 Lucidity<br /><b>Effect:</b> Strong Level 8 Cold Attack<br /><b>Result:</b> 24d6+24 (<i>Est. 108</i>) Cold Damage", 
-	3 : "<b>Cost:</b> 24 Lucidity, 16 Superego<br /><b>Effect:</b> Strong Level 8 Id Boost<br /><b>Result:</b> 8d8 (<i>Est. 36</i>) Id Boost", 
-	4 : "<b>Cost:</b> 6 Lucidity, 12 Id<br /><b>Effect:</b> Weak Level 6 Superego Boost<br /><b>Result:</b> 6d4 (<i>Est. 15</i>) Superego Boost", 
-	5 : "Invisibility", 
-};
 
 // Page initialization
 $(function () {
